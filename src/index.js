21312753ac5cdde0394d697ca0e50b91b893cb27
Revina Nataliia <natalyrevina@gmail.com>
2018-02-26 14:30:43 +0300

    Quadratic equation solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,36 @@
 module.exports = function solveEquation(equation) {
-  // your implementation
-}
+ var a;
+ var b;
+ var c;
+ var d;
+ var x1;
+ var x2;
+ var result =  equation.split(' ');
+ a = result[0];
+ b = result[3] + result[4];
+ c = result[7] + result[8];
+ console.log (result, a, b, c, d);
+ d = b*b-4*a*c;
+ if( d < 0 ) {
+console.log("Нет ответа");
+  }
+  if ( d == 0 ){
+    x1 = -b/(2*a);
+     }
+  if ( d > 0 ) {
+    x1 = Math.round((-b + Math.sqrt(d))/(2*a));
+    x2 = Math.round((-b - Math.sqrt(d))/(2*a));
+    var solution = [];
+    solution.push(x1, x2);
+    console.log (solution);
+  }
+  if (solution[0] > solution[1]){
+    var n = solution[0];
+    solution[0] = solution[1];
+    solution[1] = n;
+  }
+    return  solution;
+      }
+    
+
+
